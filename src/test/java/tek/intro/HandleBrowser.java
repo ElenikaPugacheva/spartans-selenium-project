51337477c8890e51b35b90657765a742f0ef26d5
Elenika <1991pugacheva@gmail.com>
2024-07-09 21:40:40 -0400

    working with browser activity 2

--- src/test/java/tek/intro/HandleBrowser.java
+++ src/test/java/tek/intro/HandleBrowser.java
@@ -4,8 +4,16 @@ public class HandleBrowser {
     public static void main(String[] args) {
         // open browser chrome driver
         ChromeDriver chromeDriver = new ChromeDriver();
+        // use this method to maximize the browser
         chromeDriver.manage().window().maximize();
+        // use this method to minimize the browser
+        chromeDriver.manage().window().minimize();
+        // navigate to URL
         chromeDriver.get("http://google.com");
+        // return string of application title
+        String title = chromeDriver.getTitle();
+        System.out.println(title);
+        // close the browser
         chromeDriver.quit();
     }
 }
