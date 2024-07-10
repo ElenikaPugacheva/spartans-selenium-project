72c8c10acec8d389463e98303d708065a411a8e0
Elenika <1991pugacheva@gmail.com>
2024-07-10 19:39:47 -0400

    cross browsing handling practice

--- src/test/java/CrossBrowsing/CrossBrowsing.java
+++ src/test/java/CrossBrowsing/CrossBrowsing.java
@@ -1,10 +1,12 @@
 package CrossBrowsing;
 
-import org.openqa.selenium.edge.EdgeDriver;
+//import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class CrossBrowsing {
     public static void main(String[] args) {
-        EdgeDriver driver = new EdgeDriver();
+        //EdgeDriver driver = new EdgeDriver();
+        FirefoxDriver driver = new FirefoxDriver();
 
         driver.manage().window().minimize();
         driver.get("http://google.com");
