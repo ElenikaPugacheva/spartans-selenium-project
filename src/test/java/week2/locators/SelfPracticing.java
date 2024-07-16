8fe0e7c6f77e5328cffffeb44ec1dd6830d5c690
Elenika <1991pugacheva@gmail.com>
2024-07-15 23:08:42 -0400

    solve

---
+++ src/test/java/week2/locators/SelfPracticing.java
@@ -0,0 +1,25 @@
+package week2.locators;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import java.time.Duration;
+public class SelfPracticing {
+       public static void main(String[] args) throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://fireflyworlds.com/categories/all-games/");
+        driver.manage().window().maximize();
+           WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(2));
+           wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id=\"post-9892\"]/div/div[2]/div/a")));
+        driver.findElement(By.cssSelector("#post-9892 > div > div.menu-content-pro > div > div > a:nth-child(3) > div")).click();
+           WebElement mainLogo = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#main-logo > img")));
+           if(mainLogo.isDisplayed()){
+               System.out.println("Pass");
+           }else {
+               System.out.println("Fail");
+           }
+        driver.quit();
+    }
+}
--- src/test/java/week2/locators/TagNameLocator.java
+++ src/test/java/week2/locators/TagNameLocator.java
@@ -11,9 +11,12 @@ public class TagNameLocator {
         driver.manage().window().maximize();
         driver.get("https://www.bbc.com/");
         By anchorsTag = By.tagName("a");
+
         // method findElements return list<WebElements> contains all the elements finds with By strategy
         List<WebElement> anchorsTagElm= driver.findElements(anchorsTag);
         System.out.println(anchorsTagElm.size());
+
+        // print all the links from the page
         for (WebElement element: anchorsTagElm){
             System.out.println(element.getText());
         }
@@ -22,10 +25,17 @@ public class TagNameLocator {
         List<WebElement> imageTagElm = driver.findElements(imageTag);
         System.out.println(imageTagElm.size());
 
+        // find how many buttons are there
         By buttons = By.tagName("button");
         List<WebElement> buttonElm = driver.findElements(buttons);
         System.out.println(buttonElm.size());
+
+        // print all the buttons
         for(WebElement button: buttonElm){
+            // if we want to print only buttons with the text use if statement
+            //String text = element.getText();
+            //if(!text.isEmpty())
+            //Sout(text);
             System.out.println("Button text- " + button.getText());
         }
         driver.quit();
