3e877d6547527cfe60a83d89e7a49b4ac9b89b65
Elenika <1991pugacheva@gmail.com>
2024-07-18 20:12:26 -0400

    activity phone number

--- src/test/java/week2/day_4_lab/Activity1.java
+++ src/test/java/week2/day_4_lab/Activity1.java
@@ -1,13 +1,15 @@
-package week2.day_3;
+package week2.day_4_lab;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
 import java.time.Duration;
 
-public class UploadPhoneNumberProfile {
+public class Activity1 {
     public static void main(String[] args) {
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
@@ -39,17 +41,5 @@ public class UploadPhoneNumberProfile {
 
 
         driver.quit();
-
-
-
-
-
-
-
-
-
-
-
-
     }
 }
