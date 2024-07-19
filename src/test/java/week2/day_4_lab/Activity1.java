388d764aca0ea67a7addba284e353cd96c666381
Elenika <1991pugacheva@gmail.com>
2024-07-18 20:14:01 -0400

    activity phone number

--- src/test/java/week2/day_4_lab/Activity1.java
+++ src/test/java/week2/day_4_lab/Activity1.java
@@ -1,14 +1,11 @@
 package week2.day_4_lab;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
 import java.time.Duration;
-
 public class Activity1 {
     public static void main(String[] args) {
         WebDriver driver = new ChromeDriver();
@@ -38,7 +35,6 @@ public class Activity1 {
 
         WebElement successUpdateMsg = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[contains(@class, 'Toastify__toast-body')]//div[text()='Personal Information Updated Successfully']\n")));
         System.out.println(successUpdateMsg.getText());
-
 
         driver.quit();
     }
