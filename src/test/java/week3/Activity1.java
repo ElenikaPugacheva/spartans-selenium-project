73d349c57aef3ea3f2eb8c27af61d724a1d1743f
Elenika <1991pugacheva@gmail.com>
2024-07-22 19:58:45 -0400

    activity drop down

--- src/test/java/week3/Activity1.java
+++ src/test/java/week3/Activity1.java
@@ -5,9 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
-
 import java.time.Duration;
-
 public class Activity1 {
     public static void main(String[] args) throws InterruptedException {
         WebDriver driver = new ChromeDriver();
@@ -15,8 +13,15 @@ public class Activity1 {
         driver.get("https://retail.tekschool-students.com/selenium/dropdown");
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         Thread.sleep(1000);
+
         WebElement countryElm = driver.findElement(By.id("countrySelect"));
         Select countrySelect = new Select(countryElm);
         countrySelect.selectByIndex(2);
+        Thread.sleep(1000);
+        WebElement languageElm = driver.findElement(By.id("languageSelect"));
+        Select languageSelect = new Select(languageElm);
+        languageSelect.selectByValue("Italian");
+
+
     }
 }
