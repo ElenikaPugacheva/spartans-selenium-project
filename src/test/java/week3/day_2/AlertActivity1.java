1b59a659458769721fd398f8e92a521a52375b04
Elenika <1991pugacheva@gmail.com>
2024-07-23 19:35:34 -0400

    activity 1 alert

--- src/test/java/week3/day_2/AlertActivity1.java
+++ src/test/java/week3/day_2/AlertActivity1.java
@@ -3,6 +3,7 @@ package week3.day_2;
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.time.Duration;
@@ -17,5 +18,10 @@ public class AlertActivity1 {
         Alert alert = driver.switchTo().alert();
         alert.sendKeys("Hello Elena");
         alert.accept();
+        WebElement messageElm = driver.findElement(By.id("message"));
+        String messageTxt = messageElm.getText();
+        System.out.println(messageTxt);
+        driver.quit();
+
     }
 }
